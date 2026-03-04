2085ddbb6898ec2e49012201c66015695faa0264
khushi62bansal-droid <khushi62.bansal@gmail.com>
2026-03-04 19:28:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pickle
+
+model = pickle.load(open('model.pkl', 'rb'))
+vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+
+st.title("📧 Spam Email Detector")
+
+input_text = st.text_area("Enter Email Text")
+
+if st.button("Predict"):
+    transformed = vectorizer.transform([input_text])
+    result = model.predict(transformed)[0]
+
+    if result == 1:
+        st.error("🚨 Spam Email")
+    else:
+        st.success("✅ Not Spam")
